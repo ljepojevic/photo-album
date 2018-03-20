4b5fff9407346c552b9efa0103163f9fd3145248
Milan Ljepojevic <ljepojevicm@yahoo.com>
2018-03-20 01:28:52 +0100

    Setting up

---
+++ resources/views/albums/create.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+	<h3>Create Album</h3>
+	<form method="POST" action="/albums/store" enctype="multipart/form-data">
+		@csrf
+		<div class="row">
+			<label for="name" class="small-3 columns">Name:</label>
+			<div class="small-5 columns">
+				<input type="text" name="name" placeholder="Album Name" class="right">
+				
+			</div>
+
+		</div>
+		<div class="row">
+			Description:
+			<textarea name="description" placeholder="Album Description"></textarea>
+		</div>
+		<div class="row">
+			<label for="cover_image" class="button">Upload Image</label>
+			<input class="show-for-sr" type="file" name="cover_image">
+		</div>
+		<div class="row">
+			<input class="button" type="submit" value="Create">
+		</div>
+	</form>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Photoshow</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.4.3/css/foundation.css">
+</head>
+<body>
+	@include('inc.topbar')
+	<div class="grid-container fluid">
+		@yield('content')
+	</div>
+</body>
+</html>
